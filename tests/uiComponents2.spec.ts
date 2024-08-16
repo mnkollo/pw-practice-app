96412d2b288300e1511c1297c83bbedc4e3e50f1
mnkollo <73963114+mnkollo@users.noreply.github.com>
2024-08-16 00:18:11 -0500

    Update uiComponents2.spec.ts

--- tests/uiComponents2.spec.ts
+++ tests/uiComponents2.spec.ts
@@ -25,18 +25,91 @@ test.describe('Form Layout Page', async () => {
         //locator assertion
         expect(inputValue).toEqual('Michael')
     })
-    test.describe('Checkboxes', async () => {
-        test.beforeEach('Navigate To Forms', async ({ page }) => {
-            await page.goto('http://localhost:4200/')
-            await page.getByText('Modal & Overlays').click()
-            await page.getByText('Toastr').click()
-        })
-        test('Verify 1st and 3rd Checkboxes checked', async ({ page }) => {
-            const firstRadioButton =  page.locator('label', { hasText: 'Hide on click' }).locator('.custom-checkbox')
-            const secondRadioButton =  page.locator('label', { hasText: 'Prevent arising of duplicate toast' }).locator('.custom-checkbox')
+})
+test.describe('Checkboxes', async () => {
+    test.beforeEach('Navigate To Toastr', async ({ page }) => {
+        await page.goto('http://localhost:4200/')
+        await page.getByText('Modal & Overlays').click()
+        await page.getByText('Toastr').click()
+    })
+    test('Verify 1st and 2nd Checkboxes checked then check all', async ({ page }) => {
+        const firstRadioButton = page.locator('label', { hasText: 'Hide on click' }).locator('.custom-checkbox')
+        const secondRadioButton = page.locator('label', { hasText: 'Prevent arising of duplicate toast' }).locator('.custom-checkbox')
 
-            await firstRadioButton.uncheck()
-            await secondRadioButton.check()
+        await firstRadioButton.uncheck()
+        await secondRadioButton.check()
+
+        const allCheckboxes = page.locator('.custom-checkbox')
+
+        for (const box of await allCheckboxes.all()) {
+            await box.check({ force: true })
+            expect(await box.isChecked()).toBeTruthy()
+        }
+    })
+})
+test.describe('list and dropdowns', async () => {
+    test.beforeEach('Open Site', async ({ page }) => {
+        await page.goto('http://localhost:4200/')
+    })
+    test('Validate color changes on webpage when switching dropdown', async ({ page }) => {
+        const dropDownMenu = page.locator('ngx-header nb-select')
+        await dropDownMenu.click()
+
+        // page.getByRole('list')  // when the list has a UL tag
+        // page.getByRole('listitem')  //when the list has LI tag
+
+        //validating the array
+        const optionList = page.locator('nb-option-list nb-option')
+        await expect(optionList).toHaveText(['Light', 'Dark', 'Cosmic', 'Corporate'])
+        await optionList.filter({ hasText: 'Cosmic' }).click()
+
+        const header = page.locator('nb-layout-header')
+        await expect(header).toHaveCSS('background-color', 'rgb(50, 50, 89)')
+
+        const colors = {
+            'Light': 'rgb(255, 255, 255)',
+            'Dark': 'rgb(34, 43, 69)',
+            'Cosmic': 'rgb(50, 50, 89)',
+            'Corporate': 'rgb(255, 255, 255)'
+        }
+
+        await dropDownMenu.click()
+        for (const color in colors) { // we use in because we are iterating over the keys of the object
+            await optionList.filter({ hasText: color }).click()
+            await expect(header).toHaveCSS('background-color', colors[color])
+            if(color != 'Corporate') {
+                await dropDownMenu.click()
+            }
+        }
+    })
+})
+test.describe('Tool Tip', async () => {
+    test.beforeEach('Navigate To Tool Tips Page', async ({ page }) => {
+        await page.goto('http://localhost:4200/')
+        await page.getByText('Modal & Overlays').click()
+        await page.getByText('Tooltip').click()
+    })
+    test('Validate tool tip', async ({ page }) => {
+        const tooltipCard = page.locator('nb-card',{hasText: 'Tooltip Placements'})
+        await tooltipCard.getByRole('button',{name:'TOP'}).hover()
+        const tooltip = await page.locator('nb-tooltip').textContent()
+        expect(tooltip).toEqual('This is a tooltip')
+    })
+})
+test.describe('Dialog Box', async () => {
+    test.beforeEach('Navigate To Tables Page', async ({ page }) => {
+        await page.goto('http://localhost:4200/')
+        await page.getByText('Tables & Data').click()
+        await page.locator('a',{hasText:'Smart Table'}).click()
+    })
+    test('Validate dialog Box', async ({ page }) => {
+        
+        page.on('dialog', dialog => {   // we have to use event listener
+            expect(dialog.message()).toEqual('Are you sure you want to delete?')
+            dialog.accept()
         })
+        const trashCanFirstRow = page.getByRole('table').locator('tr',{hasText: 'mdo@gmail.com'}).locator('.nb-trash')
+        await trashCanFirstRow.click()
+        await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com')
     })
 })
